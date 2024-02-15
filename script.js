f39b98d8e699ff06ff830c9f0fcfcc119beed846
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-15 10:31:58 +0100

    Edit existing function get the value of the input
    Created an oninput event to change the grid size.
    Moved getWidth into createTiles.

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function getWidth (){
-    let result = 100/pixel;
-    return `${result}%`;
-};
+function createTiles(length){
 
-function createTiles(){
+    function getWidth (length){
+        let result = 100/length;
+        return `${result}%`;
+    };
 
-    for (i = 0; i < (pixel*pixel); i++){
+    pixel.oninput = () => {
+        let value = pixel.value;
+        pixelInfo.textContent = value;
+        document.querySelectorAll(".tile").forEach(el => el.remove());
+        createTiles(value);
+    }
+
+    for (i = 0; i < (length*length); i++){
         
         let tile = document.createElement("div");
         tile.classList.add("tile");
-        tile.style.minHeight = getWidth();
-        tile.style.minWidth = getWidth();
+        tile.style.minHeight = getWidth(length);
+        tile.style.minWidth = getWidth(length);
         tile.addEventListener("mouseover", (e) => {
             e.target.style.backgroundColor = "#1F2937"
         })
@@ -18,7 +25,9 @@ function createTiles(){
     }
 };
 
-let pixel = 32;
+
+const pixelInfo = document.querySelector(".pixelInfo")
+const pixel = document.querySelector("#size");
 const box = document.querySelector(".box");
 
-createTiles();
+createTiles(pixel.value);
