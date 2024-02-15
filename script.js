f222a0d85e55d9152566aa1a83799cd702ea9fc7
davidlueke <156998496+davidlueke@users.noreply.github.com>
2024-02-15 08:26:38 +0100

    Add overall style to the page

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function getWidth (){
-    let result = 100/pixel
-    return `${result}%`
-}
+    let result = 100/pixel;
+    return `${result}%`;
+};
 
 function createTiles(){
 
     for (i = 0; i < (pixel*pixel); i++){
         
-        let tile = document.createElement("div")
-        tile.classList.add("tile")
-        tile.style.minHeight = getWidth()
-        tile.style.minWidth = getWidth()
-        box.appendChild(tile)
+        let tile = document.createElement("div");
+        tile.classList.add("tile");
+        tile.style.minHeight = getWidth();
+        tile.style.minWidth = getWidth();
+        box.appendChild(tile);
     }
-}
+};
 
-let pixel = 32
-const box = document.querySelector(".box")
+let pixel = 16;
+const box = document.querySelector(".box");
 
-createTiles()
+createTiles();
